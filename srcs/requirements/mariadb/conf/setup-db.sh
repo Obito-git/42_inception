81934693ad2a9ae29afcf42588c29f22403773fb
amyroshn <amyroshn@42.fr>
2022-07-31 11:56:42 +0200

    db script and makefile

--- srcs/requirements/mariadb/conf/setup-db.sh
+++ srcs/requirements/mariadb/conf/setup-db.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-mkdir -p /home/amyroshn/data/database
-mkdir -p /home/amyroshn/data/www
-
 /usr/bin/mysql_install_db --user=root --basedir=/usr --datadir=/var/lib/mysql
 /usr/bin/mysqld --user=root --datadir=/var/lib/mysql & sleep 2
 
